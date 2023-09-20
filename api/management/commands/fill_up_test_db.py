b2851a9657b03adfdaa5f5c2e3ce1b37fe4c658c
tuipik <tuipiku@gmail.com>
2023-09-20 20:49:07 +0300

    changed creating dev db data

--- api/management/commands/fill_up_test_db.py
+++ api/management/commands/fill_up_test_db.py
@@ -1,5 +1,10 @@
-from django.core.management.base import BaseCommand
+from datetime import datetime
+from random import choice, randint
 
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from api.CONSTANTS import ROW_LATIN_LETTERS, CYRILLIC_LETTERS_UP
 from api.models import (
     Status,
     BaseStatuses,
@@ -14,11 +19,13 @@ from api.models import (
 class Command(BaseCommand):
     """Django command to fill up develop db"""
 
+    fake = Faker()
+
     def create_user(self, dep, num):
         user = User.objects.create_user(
-            username=f"user_{dep.name}_{num}",
-            first_name=f"First_name_{dep.name}_{num}",
-            last_name=f"Last_name_{dep.name}_{num}",
+            username=f"user_{num}",
+            first_name=self.fake.unique.first_name(),
+            last_name=self.fake.unique.last_name(),
             password="qwerty",
             department=dep,
         )
@@ -28,49 +35,71 @@ class Command(BaseCommand):
     def create_departments(self):
         deps = [
             ("Dep_1", False, [1, 2]),
+            ("Dep_2", False, [1, 2]),
+            ("Dep_3", False, [1, 2]),
             ("CORRECT", True, [3, 4]),
             ("VTK", True, [5, 6]),
         ]
+        user_counter = 1
         for dep in deps:
             department = Department.objects.create(name=dep[0], is_verifier=dep[1])
             department.statuses.set(dep[2])
             department.save()
-            user_1 = self.create_user(department, 1)
-            user_2 = self.create_user(department, 2)
+            user_1 = self.create_user(department, user_counter)
+            user_counter += 1
+            user_2 = self.create_user(department, user_counter)
+            user_counter += 1
             department.head = user_1
             department.save()
             self.stdout.write(f"Created department: {department.name}")
 
     def create_tasks(self):
-        user = User.objects.filter(department__name="Dep_1").last()
+        user_list = User.objects.filter(department__is_verifier=False)
         status = Status.objects.get(name=BaseStatuses.WAITING.name)
-        department = Department.objects.get(name="Dep_1")
-        for i in range(1, 4):
-            task = Task.objects.create(
-                **{
-                    "name": f"M-36-10{i}-Б",
-                    "change_time_estimate": 55 + i,
-                    "correct_time_estimate": 54 + i,
-                    "otk_time_estimate": 53 + i,
-                    "quarter": 1,
-                    "category": "some_cat",
-                    "user": user,
-                    "department": department,
-                    "primary_department": department,
-                    "year": 2023,
-                    "status": status,
-                }
-            )
-            TimeTracker.objects.create(
-                task=task, user=User.objects.first(), task_status=status
-            )
-            Comment.objects.create(
-                task=task,
-                user=User.objects.first(),
-                body=f"Створено задачу {task.name}",
-                is_log=True,
-            )
-            self.stdout.write(f"Created task: {task.name}")
+        names = []
+        for department in Department.objects.filter(is_verifier=False):
+            for user in [
+                u
+                for u in user_list
+                if not u.is_head_department and u.department_id == department.id
+            ]:
+                for i in range(1, 4):
+                    success = False
+                    task_name = ""
+                    while not success:
+                        name = f"{choice(ROW_LATIN_LETTERS)}-{randint(32, 38)}-{randint(1, 144)}-{choice(CYRILLIC_LETTERS_UP)}"
+                        if name not in names:
+                            names.append(name)
+                            task_name = name
+                            success = True
+
+                    task = Task.objects.create(
+                        **{
+                            "name": task_name,
+                            "change_time_estimate": randint(10, 90),
+                            "correct_time_estimate": randint(10, 90),
+                            "otk_time_estimate": randint(10, 90),
+                            "quarter": 1,
+                            "category": self.fake.sentence(nb_words=6),
+                            "user": user,
+                            "department": department,
+                            "primary_department": department,
+                            "year": datetime.today().year,
+                            "status": status,
+                            "scale": 50,
+                        }
+                    )
+
+                    TimeTracker.objects.create(
+                        task=task, user=User.objects.first(), task_status=status
+                    )
+                    Comment.objects.create(
+                        task=task,
+                        user=User.objects.first(),
+                        body=f"Створено задачу {task.name}",
+                        is_log=True,
+                    )
+                    self.stdout.write(f"Created task: {task.name}")
 
     def handle(self, *args, **options):
         self.stdout.write("Start fill up db")
