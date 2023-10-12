27fcb905551b290d8ef1ca7f4edd37fc543d4c2c
Tymoshenko <tzd0409@gmail.com>
2023-10-12 11:39:24 +0300

    Fix cors

--- kanban/settings.py
+++ kanban/settings.py
@@ -47,6 +47,9 @@ CORS_ORIGIN_WHITELIST = (
        'http://127.0.0.1:3000',
        'http://0.0.0.0:8000',
        'http://0.0.0.0:3000',
+       'http://0.0.0.0:80',
+       'http://127.0.0.1',
+       'http://localhost',
 )
 CORS_ALLOW_CREDENTIALS = True
 
